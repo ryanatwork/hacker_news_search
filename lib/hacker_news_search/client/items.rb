5dde0829a1d95f17e504f9e7a3d6efb375940145
Ryan Resella <ryan@codeforamerica.org>
2011-06-05 12:52:41 -0700

    Added documentation for Items

--- lib/hacker_news_search/client/items.rb
+++ lib/hacker_news_search/client/items.rb
@@ -1,9 +1,19 @@
 module HackerNewsSearch
   class Client
     module Items
+      
+      # Returns the information of the item from Hacker News Search
+      #
+      # @param item_id [String] The hacker news item plus the thriftdb_id - must know the thriftDB id
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.hnsearch.com/api
+      # @example
+      #   HackerNewsSearch.item("2619846-a30f9")
       def item(item_id, options={})
          get("items/#{item_id}", options)
       end
+      
     end
   end
 end
