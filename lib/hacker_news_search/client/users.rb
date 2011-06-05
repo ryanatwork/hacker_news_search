30cfe2c2ad15b1000e861341802c281cf63ff03c
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 22:39:45 -0700

    Implmeneted users

--- lib/hacker_news_search/client/users.rb
+++ lib/hacker_news_search/client/users.rb
@@ -1,7 +1,10 @@
 module HackerNewsSearch
   class Client
     module Users
-
+      def users(user_name, options={})
+        get("users/#{user_name}", options)
+      end
+      
     end
   end
 end
