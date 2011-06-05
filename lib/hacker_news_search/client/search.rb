8528e6fbb46bf8b4685d8081058bd360f50ae71a
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 23:54:24 -0700

    Added documentation for users

--- lib/hacker_news_search/client/search.rb
+++ lib/hacker_news_search/client/search.rb
@@ -8,6 +8,7 @@ module HackerNewsSearch
       # @param options [Hash] A customizable set of options. 
       # @return {Hash}
       # @see http://www.hnsearch.com/api
+      # @see http://www.thriftdb.com/documentation/rest-api/search-api
       # @example
       #   HackerNewsSearch.search("users" {:q => "sferik"})
       def search(collection, options={})
--- lib/hacker_news_search/client/users.rb
+++ lib/hacker_news_search/client/users.rb
@@ -1,10 +1,18 @@
 module HackerNewsSearch
   class Client
     module Users
+      
+      # Returns the information about a user
+      #
+      # @param user_name [String] user name from Hacker News
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.hnsearch.com/api
+      # @example
+      #   HackerNewsSearch.users("sferik")
       def users(user_name, options={})
         get("users/#{user_name}", options)
-      end
-      
+      end  
     end
   end
 end
