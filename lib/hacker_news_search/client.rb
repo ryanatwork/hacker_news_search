1748c5e27d4601450379293ff5fe68a806f32eb0
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 22:31:45 -0700

    Rearragned files to the correct directories and fixed missing class declarations

--- lib/hacker_news_search/client.rb
+++ lib/hacker_news_search/client.rb
@@ -1,12 +1,14 @@
-require 'hacker_news_search/connection'
-require 'hacker_news_search/request'
-require 'hacker_news_search/items'
-require 'hacker_news_search/users'
-require 'hacker_news_search/search'
+require 'hacker_news_search/client/connection'
+require 'hacker_news_search/client/request'
+require 'hacker_news_search/client/items'
+require 'hacker_news_search/client/users'
+require 'hacker_news_search/client/search'
 
 module HackerNewsSearch
   class Client
-    include HackerNewsSearch::Connection
-    include HackerNewsSearch::Request
+    include HackerNewsSearch::Client::Connection
+    
+    include HackerNewsSearch::Client::Request
+    include HackerNewsSearch::Client::Users
   end
 end  
