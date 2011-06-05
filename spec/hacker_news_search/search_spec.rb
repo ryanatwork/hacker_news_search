a8de7512b0402c0c43ada934239f2ff629bc7230
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 23:01:09 -0700

    Wrote spec for search

---
+++ spec/hacker_news_search/search_spec.rb
@@ -0,0 +1,23 @@
+require 'helper'
+
+describe HackerNewsSearch::Client::Search do
+
+  before do
+    @client = HackerNewsSearch::Client.new
+  end
+
+  describe ".search" do
+      context "with item search" do
+        before do
+          stub_get("items/_search?q=sferik").
+            to_return(:status => 200, :body => fixture("items_search.json"))
+        end
+
+        it "should fetch all items matching the search criteria" do
+          search = @client.search("item", "sferik")
+          a_get("items/_search?q=sferik").should have_been_made
+          search.hits.should == "13"
+        end
+      end
+    end
+end
