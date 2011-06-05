9e905a59eb7642ca279948b8d3fe3935059dd95b
Ryan Resella <ryan@codeforamerica.org>
2011-06-05 12:46:39 -0700

    Added spec for items

--- spec/hacker_news_search/items_spec.rb
+++ spec/hacker_news_search/items_spec.rb
@@ -7,8 +7,15 @@ describe HackerNewsSearch::Client::Items do
   end
 
   describe ".items" do
-    it "should return the correct items" do
-      pending("get the item")
+    before do
+      stub_get("items/2619846-a30f9").
+        to_return(:status => 200, :body => fixture("items.json"))
+    end
+    
+    it "should return the correct item" do
+      item = @client.item("2619846-a30f9")
+      a_get("items/2619846-a30f9").should have_been_made
+      item.id.should == 2619846
     end
   end
 
