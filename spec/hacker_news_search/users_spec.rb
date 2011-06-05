ec35d118929c5679afa64267c3c687d29cc51dca
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 22:39:23 -0700

    Refactored users spec

--- spec/hacker_news_search/users_spec.rb
+++ spec/hacker_news_search/users_spec.rb
@@ -7,14 +7,16 @@ describe HackerNewsSearch::Client::Users do
   end
 
   describe ".users" do
+      before do
+        stub_get("users/sferik").
+          to_return(:status => 200, :body => fixture("users.json"))
+      end
 
-    it "should return a single user" do
-      stub_get("users").
-        to_return(:body => fixture("users.json"))
-      users = @client.users("sferik")
-      users.first.username.should == "sferik"
-    end
-
+      it "should return one user" do
+        user = @client.users("sferik")
+        a_get("users/sferik").should have_been_made
+        user.username.should == "sferik"
+      end
   end
 
 end
