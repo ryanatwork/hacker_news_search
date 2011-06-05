066d8d6c9a04f06b89b3b3b818a667692796bee5
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 20:05:41 -0700

    Updated spec helper file for api path

--- spec/helper.rb
+++ spec/helper.rb
@@ -4,3 +4,44 @@ require 'simplecov'
 SimpleCov.start
 require 'hacker_news_search'
 require 'rspec'
+require 'webmock/rspec'
+
+def a_delete(path)
+  a_request(:delete, 'http://api.thriftdb.com/api.hnsearch.com/' + path)
+end
+
+def a_get(path)
+  a_request(:get, 'http://api.thriftdb.com/api.hnsearch.com/' + path)
+end
+
+def a_post(path)
+  a_request(:post, 'http://api.thriftdb.com/api.hnsearch.com/' + path)
+end
+
+def a_put(path)
+  a_request(:put, 'http://api.thriftdb.com/api.hnsearch.com/' + path)
+end
+
+def stub_delete(path)
+  stub_request(:delete, 'http://api.thriftdb.com/api.hnsearch.com/' + path)
+end
+
+def stub_get(path)
+  stub_request(:get, 'http://api.thriftdb.com/api.hnsearch.com/' + path)
+end
+
+def stub_post(path)
+  stub_request(:post, 'http://api.thriftdb.com/api.hnsearch.com/' + path)
+end
+
+def stub_put(path)
+  stub_request(:put, 'http://api.thriftdb.com/api.hnsearch.com/' + path)
+end
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
