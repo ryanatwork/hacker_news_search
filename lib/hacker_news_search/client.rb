50110c4239a373766dc2688739de3b6ac5395ae8
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 20:12:39 -0700

    Added file for client

---
+++ lib/hacker_news_search/client.rb
@@ -0,0 +1,9 @@
+require 'hacker_news_search/connection'
+require 'hacker_news_search/request'
+
+module HackerNewsSearch
+  class Client
+    include HackerNewsSearch::Connection
+    include HackerNewsSearch::Request
+
+end
