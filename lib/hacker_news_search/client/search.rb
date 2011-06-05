9e37a8259192da2e4a29ef1c3bf353afaa1ac657
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 23:51:54 -0700

    Wrote documentation for search

--- lib/hacker_news_search/client/search.rb
+++ lib/hacker_news_search/client/search.rb
@@ -1,6 +1,15 @@
 module HackerNewsSearch
   class Client
     module Search
+      
+      # Returns the information based on the search criteria
+      #
+      # @param collection [String] collection is either -  users or items 
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.hnsearch.com/api
+      # @example
+      #   HackerNewsSearch.search("users" {:q => "sferik"})
       def search(collection, options={})
         get("#{collection}/_search", options)
       end
