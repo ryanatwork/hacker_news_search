4f075f5bf8acf48e007ef1c28341bd47176c6396
Ryan Resella <ryan@codeforamerica.org>
2011-06-04 20:21:40 -0700

    Added hacker_news_search.rb file

--- lib/hacker_news_search.rb
+++ lib/hacker_news_search.rb
@@ -1,3 +1,20 @@
+require 'hacker_news_search/client'
+
 module HackerNewsSearch
-  # Your code goes here...
+  # Alias for HackerNewsSearch::Client.new
+  #
+  # @return [HackerNewsSearch::Client]
+  def self.new
+    HackerNewsSearch::Client.new
+  end
+
+  # Delegate to HackerNewsSearch::Client
+  def self.method_missing(method, *args, &block)
+    return super unless new.respond_to?(method)
+    new.send(method, *args, &block)
+  end
+
+  def self.respond_to?(method, include_private=false)
+    new.respond_to?(method, include_private) || super(method, include_private)
+  end
 end
